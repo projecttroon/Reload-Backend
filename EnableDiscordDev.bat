b6749b5ca220e5a0db57303751ca8da86e243d88
projecttroonogfn <rd7696251@gmail.com>
2026-03-07 08:59:06 -0300

    Simplify EnableDiscordDev.bat script
    Removed the process termination and configuration modification for enabling Developer Mode in Discord. Updated the script to only open the Developer Mode settings.

--- EnableDiscordDev.bat
+++ EnableDiscordDev.bat
@@ -1,22 +1,6 @@
 @echo off
-echo Enabling Discord Developer Mode...
+echo Opening Discord Developer Mode settings...
 
-
-taskkill /F /IM discord.exe >nul 2>&1
-taskkill /F /IM discordcanary.exe >nul 2>&1
-taskkill /F /IM discordptb.exe >nul 2>&1
-
-set "config=%APPDATA%\discord\settings.json"
-
-if not exist "%config%" (
-echo { "developerMode": true } > "%config%"
-) else (
-powershell -Command "(Get-Content '%config%') -replace '\"developerMode\": false','\"developerMode\": true' | Set-Content '%config%'"
-)
-
-echo Developer Mode enabled.
-echo Launching Discord...
-
-start "" "%LOCALAPPDATA%\Discord\Update.exe" --processStart Discord.exe
+start discord://-/settings/advanced
 
 pause
