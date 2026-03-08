34206d1052bb840afecbf31668a886abb19891c6
projecttroonogfn <rd7696251@gmail.com>
2026-03-08 13:46:54 -0300

    Replace process.env with config for game server IP

--- routes/matchmaking.js
+++ routes/matchmaking.js
@@ -7,7 +7,6 @@ const MMCode = require("../model/mmcodes.js");
 const { verifyToken } = require("../tokenManager/tokenVerify.js");
 const qs = require("qs");
 const error = require("../structs/error.js");
-require("dotenv").config();
 
 let buildUniqueId = {};
 
@@ -30,7 +29,7 @@ app.get("/fortnite/api/game/v2/matchmakingservice/ticket/player/*", verifyToken,
     const rawPlaylist = bucketId.split(":")[3];
     let playlist = functions.PlaylistNames(rawPlaylist).toLowerCase();
 
-    const gameServers = process.env.GAME_SERVER_IP;
+    const gameServers = config.gameServerIP;
     let selectedServer = gameServers.find(server => server.split(":")[2].toLowerCase() === playlist);
     if (!selectedServer) {
         log.debug("No server found for playlist", playlist);
@@ -78,7 +77,7 @@ app.get("/fortnite/api/matchmaking/session/:sessionId", verifyToken, async (req,
     const playlist = await global.kv.get(`playerPlaylist:${req.user.accountId}`);
     let kvDocument = await global.kv.get(`playerCustomKey:${req.user.accountId}`);
     if (!kvDocument) {
-        const gameServers = process.env.GAME_SERVER_IP;
+        const gameServers = config.gameServerIP;
         let selectedServer = gameServers.find(server => server.split(":")[2] === playlist);
         if (!selectedServer) {
             log.debug("No server found for playlist", playlist);
